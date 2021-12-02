8567b88e0b29188d12c1de2e7a735185ec697d39
ulysses@bund-subsystem <wagren@kth.se>
2021-12-02 22:36:28 +0100

    return answer and approximate time to execute

--- aoc/solutions.py
+++ aoc/solutions.py
@@ -2,7 +2,7 @@
 which invokes the attribute of the class relating to the respective function.
 all functions implementing a solution to an AoC problem should have the format
 
-    `fYEARdDAYpPART` 
+    `f<YEAR>d<DAY>p<PART>` 
 
 and expect one argument, the data which as been read from the corresponding 
 problem datafile. the input data is a list of strings, one string for each
@@ -48,29 +48,37 @@ class Solutions(object):
         pass
 
     def f2020d1p1(self, data):
+        t_start = time.perf_counter_ns()
         data = _intify(data)
         for id1, d1 in enumerate(data):
             for id2, d2 in enumerate(data[id1:]):
                 if d1+d2==2020:
-                    return d1*d2
+                    t_stop = time.perf_counter_ns()
+                    return (d1*d2, t_stop-t_start)
 
     def f2020d1p2(self, data):
+        t_start = time.perf_counter_ns()
         data = _intify(data)
         for id1, d1 in enumerate(data):
             for id2, d2 in enumerate(data[id1:]):
                 for id3, d3 in enumerate(data[id2:]):
                     if d1+d2+d3==2020:
-                        return d1*d2*d3
+                        t_stop = time.perf_counter_ns()
+                        return (d1*d2*d3, t_stop-t_start)
 
     def f2020d2p1(self, data):
+        t_start = time.perf_counter_ns()
         def _valid(num, char, pwd):
             count = Counter(pwd)
             if num[0] <= count.get(char, 0) <= num[1]:
                 return 1
             return 0
-        return sum(list(_valid(nums, char, pwd) for nums, char, pwd in _format(data)))
+        answer = sum(list(_valid(nums, char, pwd) for nums, char, pwd in _format(data)))
+        t_stop = time.perf_counter_ns()
+        return (answer, t_stop-t_start)
     
     def f2020d2p2(self, data):
+        t_start = time.perf_counter_ns()
         def _valid(nums, char, pwd):
             num1, num2 = nums
             if pwd[num1-1] is char and pwd[num2-1] is char:
@@ -80,34 +88,52 @@ class Solutions(object):
             elif pwd[num2-1] is char:
                 return 1
             return 0
-        return sum(list(_valid(nums, char, pwd) for nums, char, pwd in _format(data)))
+        answer = sum(list(_valid(nums, char, pwd) for nums, char, pwd in _format(data)))
+        t_stop = time.perf_counter_ns()
+        return (answer, t_stop-t_start)
 
     def f2020d3p1(self, data):
-        return _traverse((data, 1, 3))
+        t_start = time.perf_counter_ns()
+        answer = _traverse((data, 1, 3))
+        t_stop = time.perf_counter_ns()
+        return (answer, t_stop-t_start)
 
     def f2020d3p2(self, data):
+        t_start = time.perf_counter_ns()
         setups = [(data, 1, 1), (data, 1, 3), (data, 1, 5), (data, 1, 7), (data, 2, 1)]
         def _mul(iterable):
             prod = 1
             for i in iterable:
                 prod *= i
             return prod
-        return _mul(list(map(_traverse, setups)))
+        answer = _mul(list(map(_traverse, setups)))
+        t_stop = time.perf_counter_ns()
+        return (answer, t_stop-t_start)
 
     def f2021d1p1(self, data):
-        return sum(list(map(lambda x: 1 if int(data[x]) > int(data[x-1]) else 0, range(1, len(data)))))
+        t_start = time.perf_counter_ns()
+        answer = sum(list(map(lambda x: 1 if int(data[x]) > int(data[x-1]) else 0, range(1, len(data)))))
+        t_stop = time.perf_counter_ns()
+        return (answer, t_stop-t_start)
 
     def f2021d1p2(self, data):
-        return sum(list(map(lambda x: 1 if int(data[x]) + int(data[x-1]) + int(data[x-2]) > int(data[x-1]) + int(data[x-2]) + int(data[x-3]) else 0, range(3, len(data)))))
+        t_start = time.perf_counter_ns()
+        answer = sum(list(map(lambda x: 1 if int(data[x]) + int(data[x-1]) + int(data[x-2]) > int(data[x-1]) + int(data[x-2]) + int(data[x-3]) else 0, range(3, len(data)))))
+        t_stop = time.perf_counter_ns()
+        return (answer, t_stop-t_start)
 
     def f2021d2p1(self, data):
+        t_start = time.perf_counter_ns()
         mappings = {'forward': 0, 'down': 0, 'up': 0}
         for line in data:
             direction, amount = line.split(' ')
             mappings[direction] += int(amount)
-        return (mappings['down']-mappings['up'])*mappings['forward']
+        answer = (mappings['down']-mappings['up'])*mappings['forward']
+        t_stop = time.perf_counter_ns()
+        return (answer, t_stop-t_start)
 
     def f2021d2p2(self, data):
+        t_start = time.perf_counter_ns()
         mappings = {'aim': 0, 'y': 0, 'x': 0}
         for line in data:
             direction, amount = line.split(' ')
@@ -118,11 +144,19 @@ class Solutions(object):
                 mappings['aim'] -= int(amount)
             elif direction == 'down':
                 mappings['aim'] += int(amount)
-        return (mappings['y'] * mappings['x'])
+        answer = mappings['y'] * mappings['x']
+        t_stop = time.perf_counter_ns()
+        return (answer, t_stop-t_start)
 
     def f2021d3p1(self, data):
-        raise NotImplementedError
+        t_start = time.perf_counter_ns()
+        answer = None
+        t_stop = time.perf_counter_ns()
+        return (answer, t_stop-t_start)
 
     def f2021d3p2(self, data):
-        raise NotImplementedError
+        t_start = time.perf_counter_ns()
+        answer = None
+        t_stop = time.perf_counter_ns()
+        return (answer, t_stop-t_start)
 
