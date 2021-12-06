a0876861aec95537dae8857ff905df2d2a1ccf42
ulysses <wagren@kth.se>
2021-12-06 16:00:00 +0100

    requests get the year-day input file given user session token

---
+++ adventofcode/datautil.py
@@ -0,0 +1,43 @@
+import os
+
+from pathlib import Path
+from requests import get, HTTPError
+from .utils import printer
+
+
+def request(year, day):
+    root = Path(__file__).parent.parent
+    session_token = Path(root, 'session.token')
+    data_path = Path(root, 'data', year, f'd{day}.in')
+
+    if data_path.exists():
+        printer.WARNING(f'data file for {year=} {day=} already exists!')
+        return
+    else:
+        printer.WORKING(f'requested data file for {year=} {day=} doesn`t exists, touching it ...')
+        data_path.touch()
+
+    try:
+        with open(session_token) as f:
+            cookie = dict(session=f.read().strip())
+    except FileNotFoundError:
+        printer.ERROR(f'no session token found in root directory ...')
+        raise
+
+    url = f'https://adventofcode.com/{year}/day/{day}/input'
+    printer.WORKING(f'getting input file ...')
+    response = get(url, cookies=cookie)
+
+    try:
+        response.raise_for_status()
+    except HTTPError as e:
+        printer.ERROR(f'something went wrong, trace: {e}')
+
+    printer.WORKING(f'writing input contents to {data_path} ...')
+    data = response.text.strip()
+    with open(data_path, 'w') as f:
+        for line in data:
+            f.write(line)
+        f.write('\n')
+    printer.WORKING(f'done getting {year=} {day=} input file!')
+
