c6f3114050fe2b21863ddd71abeee215d974ba7a
ulysses@bund-subsystem <wagren@kth.se>
2021-12-08 07:57:58 +0100

    Day 7&8 solved and commited

--- aoc/solver.py
+++ aoc/solver.py
@@ -364,3 +364,82 @@ class Solutions:
         t_stop = time.perf_counter_ns()
         return (answer, t_stop-t_start)
 
+    def f2021d8p1(self, data):
+        t_start = time.perf_counter_ns()
+        sumofnums = 0
+        for line in data:
+            line = line.split(' ')
+            for string in line[11:]:
+                lent = len(string)
+                if lent == 2 or lent == 4 or lent == 3 or lent == 7:
+                    sumofnums += 1
+        answer = sumofnums
+        t_stop = time.perf_counter_ns()
+        return (answer, t_stop-t_start)
+    
+    def f2021d8p2(self, data):
+        from itertools import permutations
+        t_start = time.perf_counter_ns()
+        numbers=list()
+
+        for idx, line in enumerate(data):
+            line = line.split(' ')
+            mapping = dict()
+            stack = list()
+            for string in line[:10]:
+                stack.append(string)
+            stack.sort(key=len)
+            kukone = [[0,1],[1,0]]
+            fouriter = [[1,3], [3,1]]
+            eightiter = [[4,6], [6,4]]
+            breaker = False
+            breakerb = False
+            for hora in kukone:
+                if breakerb:
+                    break
+                for xdfour in fouriter:
+                    if breaker:
+                        break
+                    for xdeight in eightiter:
+                        for idx, string in enumerate(stack):
+                            if idx == 0:
+                                xd1, xd2 = hora
+                                mapping[2] = string[xd1]
+                                mapping[5] = string[xd2]
+                            if idx == 1:
+                                mapping[0] = string.replace(mapping[2],'').replace(mapping[5],'')
+                            if idx == 2:
+                                for kuk, s in zip(xdfour, string.replace(mapping[2],'').replace(mapping[5],'')):
+                                    mapping[kuk] = s
+                            if idx == 9:
+                                for kuk, s in zip(xdeight, string.replace(mapping[2],'').replace(mapping[5],'').replace(mapping[1],'').replace(mapping[3],'').replace(mapping[0],'')):
+                                    mapping[kuk] = s
+                        number = ''
+                        zero = mapping[0]+mapping[1]+mapping[2]+mapping[4]+mapping[5]+mapping[6]
+                        one = mapping[2]+mapping[5]
+                        two = mapping[0]+mapping[2]+mapping[3]+mapping[4]+mapping[6]
+                        three = mapping[0]+mapping[2]+mapping[3]+mapping[5]+mapping[6]
+                        four = mapping[1]+mapping[2]+mapping[3]+mapping[5]
+                        five = mapping[0]+mapping[1]+mapping[3]+mapping[5]+mapping[6]
+                        six = mapping[0]+mapping[1]+mapping[3]+mapping[4]+mapping[5]+mapping[6]
+                        seven = mapping[0]+mapping[2]+mapping[5]
+                        eight = ''.join(mapping.values())
+                        nine = ''.join(mapping.values()).replace(mapping[4],'')
+                        numbermap = {zero:'0',one:'1', two:'2', three:'3', four:'4', five:'5', six:'6',seven:'7',eight:'8',nine:'9'}
+                        num = ''
+                        for string in line[11:]:
+                            for perm in [''.join(p) for p in permutations(string)]:
+                                val = numbermap.get(perm, None)
+                                if val:
+                                    num += val
+                                    break
+                        if len(num) == 4:
+                            breaker = True
+                            breakerb = True
+                            numbers.append(num)
+                            break
+
+        answer = sum(map(lambda x: int(x), numbers))
+        t_stop = time.perf_counter_ns()
+        return (answer, t_stop-t_start)
+
