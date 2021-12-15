6897b5126067e9b45a3cbbc10ceedf29be14e305
Eli Vance <wagren@kth.se>
2021-12-15 12:28:11 +0100

    Copium Astar

--- solutions/2021/d15p1.py
+++ solutions/2021/d15p1.py
@@ -8,6 +8,96 @@ import fileinput as fi
 from collections import defaultdict
 
 
+def pythagorean(y, ey, x, ex):
+    return (y-ey)**2 + (x-ex)**2
+
+
+class Node():
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.f = 0
+        self.g = 0
+        self.h = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+
+def bounds(arg, HEIGHT, WIDTH):
+    y,x = arg
+    return (0<=y) and (y<HEIGHT) and (0<=x) and (x<WIDTH)
+
+
+def astar(graph, start, end):
+    HEIGHT = len(graph)
+    WIDTH = len(graph[0])
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+    openlist = []
+    closedlist = []
+    openlist.append(start_node)
+
+    while openlist:
+        curr = openlist[0]
+        curr_idx = 0
+    
+        # Get the node in open set that has shortest theoretical path to end
+        for idx, node in enumerate(openlist):
+            if node.f < curr.f:
+                curr = node
+                curr_idx = idx
+    
+        # Mark the current node with shortest distance as explored, move to closed
+        openlist.pop(curr_idx)
+        closedlist.append(curr)
+
+        if curr == end_node:
+            path = []
+            while curr:
+                path.append(curr.position)
+                curr = curr.parent
+            return path[::-1]
+        
+        # Generate children
+        children = []
+
+        adjacents = [(-1,0), (0,-1), (0,1), (1,0)]                
+        for adj in adjacents:
+            npos = (curr.position[0]+adj[0], curr.position[1]+adj[1])
+
+            if not bounds(npos, HEIGHT, WIDTH):
+                continue
+
+            children.append(Node(curr, npos))
+        
+        for child in children:
+            for closed in closedlist:
+                if closed == child:
+                    continue
+
+            # Calculate heuristical distances
+            child.g = curr.g + 1
+            """
+            child.h = pythagorean(child.position[0], end_node.position[0], child.position[1],
+                    end_node.position[1])
+            """
+            child.h = int(graph[child.position[0]][child.position[1]])
+            child.f = child.g + child.h
+
+            for opened in openlist:
+                if child == opened and child.g > opened.g:
+                    continue
+
+            openlist.append(child)
+
+
+
+
 class Graph:
     def __init__(self, data):
         self._setup(data)
@@ -39,9 +129,6 @@ class Graph:
     def _bounds(self, *args):
         y,x = args
         return (0<=y) and (y<self.HEIGHT) and (0<=x) and (x<self.WIDTH)
-    
-    def astar(self, source=None, target=None):
-        pass
     
     def dijkstra(self, source=None, target=None):
         from queue import PriorityQueue
@@ -78,16 +165,16 @@ class Graph:
 
 def run(data, io_time):
     t_start = time.perf_counter_ns()
-    G = Graph(data)
-    answer = int(G.dijkstra())
+    path = astar(data, (0,0,), (9,9))
+    answer = None
+    print(path)
     t_end = time.perf_counter_ns()
     sys.stdout.write(f'{answer} {io_time} {t_end-t_start}')
 
 if __name__ == '__main__':
     t_start = time.perf_counter_ns()
-    data = [line.rstrip() for line in fi.input()]
-    """
-    data = "1163751742
+    #data = [line.rstrip() for line in fi.input()]
+    data = """1163751742
 1381373672
 2136511328
 3694931569
@@ -96,8 +183,7 @@ if __name__ == '__main__':
 1359912421
 3125421639
 1293138521
-2311944581".split('\n')
-    """
+2311944581""".split('\n')
     t_stop = time.perf_counter_ns()
     run(data, t_stop-t_start)
 
