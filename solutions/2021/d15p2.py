d13f072f774c90aed591a61bd255421fbe11db21
Eli Vance <wagren@kth.se>
2021-12-15 13:08:42 +0100

    i need copium :star:

--- solutions/2021/d15p2.py
+++ solutions/2021/d15p2.py
@@ -8,100 +8,31 @@ import fileinput as fi
 from collections import defaultdict
 
 
-def pythagorean(y, ey, x, ex):
-    return (y-ey)**2 + (x-ex)**2
-
-
-class Node():
-    def __init__(self, parent=None, position=None):
-        self.parent = parent
-        self.position = position
-
-        self.f = 0
-        self.g = 0
-        self.h = 0
-
-    def __eq__(self, other):
-        return self.position == other.position
-
-
-def bounds(arg, HEIGHT, WIDTH):
-    y,x = arg
-    return (0<=y) and (y<HEIGHT) and (0<=x) and (x<WIDTH)
-
-
-def astar(graph, start, end):
-    HEIGHT = len(graph)
-    WIDTH = len(graph[0])
-    start_node = Node(None, start)
-    start_node.g = start_node.h = start_node.f = 0
-    end_node = Node(None, end)
-    end_node.g = end_node.h = end_node.f = 0
-
-    openlist = []
-    closedlist = []
-    openlist.append(start_node)
-
-    while openlist:
-        curr = openlist[0]
-        curr_idx = 0
-    
-        # Get the node in open set that has shortest theoretical path to end
-        for idx, node in enumerate(openlist):
-            if node.f < curr.f:
-                curr = node
-                curr_idx = idx
-    
-        # Mark the current node with shortest distance as explored, move to closed
-        openlist.pop(curr_idx)
-        closedlist.append(curr)
-
-        if curr == end_node:
-            path = []
-            while curr:
-                path.append(curr.position)
-                curr = curr.parent
-            return path[::-1]
-        
-        # Generate children
-        children = []
-
-        adjacents = [(-1,0), (0,-1), (0,1), (1,0)]                
-        for adj in adjacents:
-            npos = (curr.position[0]+adj[0], curr.position[1]+adj[1])
-
-            if not bounds(npos, HEIGHT, WIDTH):
-                continue
-
-            children.append(Node(curr, npos))
-        
-        for child in children:
-            for closed in closedlist:
-                if closed == child:
-                    continue
-
-            # Calculate heuristical distances
-            child.g = curr.g + 1
-            """
-            child.h = pythagorean(child.position[0], end_node.position[0], child.position[1],
-                    end_node.position[1])
-            """
-            child.h = int(graph[child.position[0]][child.position[1]])
-            print(child.h)
-            child.f = child.g + child.h
-
-            for opened in openlist:
-                if child == opened and child.g > opened.g:
-                    continue
-
-            openlist.append(child)
-
-
-
-
 class Graph:
     def __init__(self, data):
+        data = self._penta(data)
         self._setup(data)
+
+    def _penta(self, data):
+        WIDTH = len(data[0])
+        lines = []
+        for line in data:
+            curr = []
+            for char in line:
+                curr.append(int(char))
+            for idx in range(4):
+                tmp = []
+                lidx, ridx = idx*WIDTH, (idx+1)*WIDTH
+                for char in curr[idx:ridx]:
+                    char = int(char) + 1
+                    if char > 9:
+                        tmp.append(1)
+                    else:
+                        tmp.append(char)
+                print(tmp)
+                curr += tmp
+            exit()
+            lines.append(curr)
 
     def _setup(self, data):
         # The argument of a defaultdict will be called when you try to access a key that 
@@ -173,7 +104,17 @@ def run(data, io_time):
 
 if __name__ == '__main__':
     t_start = time.perf_counter_ns()
-    data = list(line.rstrip() for line in fi.input())
+    #data = list(line.rstrip() for line in fi.input())
+    data = """1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581""".split('\n')
     t_stop = time.perf_counter_ns()
     run(data, t_stop-t_start)
 
