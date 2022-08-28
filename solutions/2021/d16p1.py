74545aa128e2399e4b638cb5fcefe56a77867f86
Eli Vance <wagren@kth.se>
2022-08-28 21:19:33 +0200

    Remove some deprecated files, prepare for new feature/rewrite branch.

---
+++ solutions/2021/d16p1.py
@@ -0,0 +1,19 @@
+"""Template for the adventofcode package pipeline.
+Replace answer=None with your solution.
+"""
+import time
+import sys
+import fileinput as fi
+
+def run(data, io_time):
+    t_start = time.perf_counter_ns()
+    answer = None
+    t_end = time.perf_counter_ns()
+    sys.stdout.write(f'{answer} {io_time} {t_end-t_start}')
+
+if __name__ == '__main__':
+    t_start = time.perf_counter_ns()
+    data = list(line.rstrip() for line in fi.input())
+    t_stop = time.perf_counter_ns()
+    run(data, t_stop-t_start)
+
